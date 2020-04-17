12c54333cad676bb2afb64f6bb855934c00202dc
Nox <juan.angarita.11@gmail.com>
2020-04-17 11:50:31 -0500

    Fix responsive

--- src/components/CollectionCard/index.tsx
+++ src/components/CollectionCard/index.tsx
@@ -3,20 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faFilm, faDragon, faDumbbell } from '@fortawesome/free-solid-svg-icons';
 import { Collection } from '@src/services/database/collections';
 import Link from 'next/link';
+import useWindowWidth from '@src/hooks/useWindowWidth';
 
 interface CollectionCardProps {
   collection: Collection;
 }
 
 const CollectionCard: React.FC<CollectionCardProps> = (props) => {
+  const width = useWindowWidth();
   return (
     <div className="my-1 p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 lg:my-4 xl:w-1/5">
-      <article className="overflow-hidden rounded-lg shadow-lg bg-purple-900">
+      <article className="overflow-hidden rounded-lg shadow-lg bg-purple-900 w-full">
         <div
           className="bg-center bg-cover bg-no-repeat"
           style={{
             backgroundImage: `url(${props?.collection.imageUrl})`,
-            height: 350,
+            height: width > 768 ? 350 : 420,
           }}
         />
         <header className="flex items-center justify-between p-2 md:p-4 uppercase">
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,7 +5,7 @@ const Layout: React.FC = (props) => {
   return (
     <div className="m-0 flex flex-col w-screen">
       <Header />
-      <main className="container mx-auto">{props.children}</main>
+      <main className="container mx-auto w-full">{props.children}</main>
     </div>
   );
 };
