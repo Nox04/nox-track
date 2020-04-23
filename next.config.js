6c707617b47e97cec1f420a99917271629253a53
Nox <juan.angarita.11@gmail.com>
2020-04-23 17:24:21 -0500

    Remove generation

--- next.config.js
+++ next.config.js
@@ -9,23 +9,4 @@ module.exports = {
   env: ENV_VARS,
   publicRuntimeConfig: ENV_VARS,
   poweredByHeader: false,
-  async exportPathMap(defaultPathMap) {
-    const pathMap = {
-      '/': { page: '/index' },
-      '/about': { page: '/about' },
-      '/login': { page: '/login' },
-      '/profile': { page: '/profile' },
-      '/collections': { page: '/index' },
-      '/login/success': { page: '/login/success' },
-      '/login/failure': { page: '/login/failure' },
-    };
-    // const collections = await getAllCollections();
-    // collections.map((collection) => {
-    //   pathMap[`/collection/${collection.slug}`] = {
-    //     page: '/collection/[slug]',
-    //     query: { slug: collection.slug },
-    //   };
-    // });
-    return pathMap;
-  },
 };
