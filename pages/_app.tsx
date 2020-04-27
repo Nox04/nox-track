f646e982bf45c667d6043628d8bfc0f71b90cbb4
Nox <juan.angarita.11@gmail.com>
2020-04-27 13:32:24 -0500

    Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,15 @@ import '@src/css/tailwind.css';
 import Head from 'next/head';
 import { AuthContextProvider } from '@src/contexts/AuthContext';
 import { SITE_NAME } from '@src/shared/constants';
+import { initGA, logPageView } from '@src/services/google-analytics.service';
+import { Router } from 'next/router';
 
 class MyApp extends ParentApp {
+  componentDidMount() {
+    initGA();
+    logPageView();
+    Router.events.on('routeChangeComplete', logPageView);
+  }
   render() {
     const { Component, pageProps } = this.props;
     return (
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,2 +1,3 @@
 export const BACKEND_URL = process.env.BACKEND_URL;
 export const SITE_NAME = process.env.SITE_NAME;
+export const GA_ID: string = process.env.GA_ID || '';
